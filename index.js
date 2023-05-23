dbba5e8d51bb511e47c38f78a80f5bbec831998b
Di3goVM <diego.vdm94@gmail.com>
2023-05-23 12:20:51 -0300

    agregar carruzel en login y mejora de interface de menu

--- index.js
+++ index.js
@@ -15,6 +15,40 @@ ingresar.onclick = function(){
 	}
 };
 
+//carrusel
+
+var slideIndex = 0;
+var slides = document.getElementsByClassName("deslizar")[0].getElementsByTagName("img");
+
+function showSlide() {
+  for (var i = 0; i < slides.length; i++) {
+  	slides[i].style.display = "none";
+    slides[i].classList.remove("activo");
+  }
+  slides[slideIndex].classList.add("activo");
+  slides[slideIndex].style.display = "block";
+}
+
+function nextSlide() {
+  slideIndex++;
+  if (slideIndex >= slides.length) {
+    slideIndex = 0;
+  }
+  showSlide();
+}
+
+function prevSlide() {
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = slides.length - 1;
+  }
+  showSlide();
+}
+
+showSlide();
+
+const carruselAutomatico = setInterval(()=>nextSlide(),5000);
+
 
 
 
